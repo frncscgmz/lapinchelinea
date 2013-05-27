ec39baedced6676cb888b5d378db42f1907ff263
Francisco Gomez <mail@frncscgmz.me>
2013-05-27 00:38:38 -0700

    buscarPuerto encapsula busqueda de xml

--- lapinchelinea.py
+++ lapinchelinea.py
@@ -17,7 +17,6 @@ def show_index():
 
 @app.route('/puerto/',methods=['POST'])
 def search_port():
-   mensaje="NO ENCONTRE NI MADRES"
    if request.form['puertos']:
       puerto = request.form['puertos']
    tipolinea='passenger_vehicle_lanes'
@@ -34,50 +33,73 @@ def search_port():
 
    portlist = tree.findall("port")
    if portlist != None:
-      for port in portlist:
-         if port.findtext("port_name") == puerto and \
-               port.findtext("port_status") == 'Open': 
-            if port.find(tipolinea).find("standard_lanes")\
-            .findtext("operational_status") == 'Update Pending':
-               return render_template('resultado.html',\
-                     mensaje="NO ENCONTRE NI MADRES")
-            tiempo = port.find(tipolinea)\
-            .find("standard_lanes")\
-            .findtext("delay_minutes")
-            linabr = port.find(tipolinea)\
-            .find("standard_lanes")\
-            .findtext("lanes_open")
-            update = port.find(tipolinea)\
-            .find("standard_lanes")\
-            .findtext("update_time")
-            if not tiempo:
-               return render_template('resultado.html',\
-                     mensaje="NO ENCONTRE NI MADRES")
-            mins = getMin(tiempo)
-            if mins <= 20:
-               mensaje="NO HAY NADA DE PINCHE LINEA"
-            elif mins > 20 and mins < 60:
-               mensaje="NO HAY TANTA PINCHE LINEA"
-            elif mins > 60 and mins < 90:
-               mensaje="ESTA LARGA LA CHINGADA LINEA"
-            elif mins > 90 and mins < 120:
-               mensaje="ESTA BIEN LARGA LA PINCHE LINEA"
-            elif mins > 120 and mins < 180:
-               mensaje="ESTA HASTA LA QUINTA CHINGADA"
-            elif mins > 180:
-               mensaje="A LA VERGA"
-               
-            return render_template('resultado.html',\
-                  puerto=puerto,tiempo=tiempo,\
-                  linabr=linabr,mensaje=mensaje,\
-                  mins=mins,update=update)
+      pinf = buscarPuerto(puerto,tipolinea,portlist)
+
+      if len(pinf) > 0:
+         return render_template('resultado.html',\
+               puerto=puerto,tiempo=pinf[0],\
+               linabr=pinf[1],mensaje=pinf[3],\
+               mins=pinf[4],update=pinf[2])
+      else:
+         return render_template('resultado.html',\
+            mensaje="NO ENCONTRE NI MADRES")
          
 def getMin(s):
    l = s.split(' ')
    if len(l) > 2:
       return int(l[0]) * 60 + int(l[2])
+   elif 'hrs' in l:
+      return int(l[0]) * 60
    else:
       return int(l[0])
+
+def buscarPuerto(puerto,tipolinea,portlist):
+   found = None
+   for port in portlist:
+      if port.findtext("port_name") == puerto and \
+      port.findtext("port_status") == 'Open': 
+         print 'Entro'
+         if port.find(tipolinea)\
+         .find("standard_lanes")\
+         .findtext("operational_status")\
+         == 'Update Pending':
+            return tuple()
+         tiempo = port.find(tipolinea)\
+         .find("standard_lanes")\
+         .findtext("delay_minutes")
+         print tiempo
+         linabr = port.find(tipolinea)\
+         .find("standard_lanes")\
+         .findtext("lanes_open")
+         print linabr
+         update = port.find(tipolinea)\
+         .find("standard_lanes")\
+         .findtext("update_time")
+         print update
+         mins = getMin(tiempo)
+         print mins
+         if mins <= 20:
+            mensaje="NO HAY NADA DE PINCHE LINEA"
+         elif mins > 20 and mins <= 60:
+            mensaje="ESTA ALGO LARGA LA PINCHE LINEA"
+         elif mins > 60 and mins <= 90:
+            mensaje="ESTA LARGA LA CHINGADA LINEA"
+         elif mins > 90 and mins <= 120:
+            mensaje="ESTA BIEN LARGA LA PINCHE LINEA"
+         elif mins > 120 and mins <= 180:
+            mensaje="ESTA HASTA LA QUINTA CHINGADA"
+         elif mins > 180:
+            mensaje="A LA VERGA"
+         else:
+            mensaje="NO ENCONTRE NI MADRES"
+         print mensaje
+         found = True
+         break
+   if not found:
+      #mensaje = "NO ENCONTRE NI MADRES"
+      #return mensaje
+      return tuple()
+   return tiempo,linabr,update,mensaje,mins
 
 if __name__=='__main__':
    app.run()
